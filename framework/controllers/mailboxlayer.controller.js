c971492404d92b7515f1dd0f7656999f49bf585b
lyarullina <lyarullina@kazanworkout.ru>
2022-06-15 14:05:22 +0300

    homework 10 ready

--- framework/controllers/mailboxlayer.controller.js
+++ framework/controllers/mailboxlayer.controller.js
@@ -4,26 +4,9 @@ import params from "../config/data";
 
 
 const Mailboxlayer = {
-    // postOfKey: async () => {
-    //     const r = await supertest(`${urls.mailboxlayer}`).post(`api/check?${params.key}`).set("Accept", "application/json");
-    //     return r;
-    // },
-    // postOfEmail: async () => {
-    //     const r = await supertest(`${urls.mailboxlayer}`).post(`api/check?${params.email}`).set("Accept", "application/json");
-    //     return r;
-    // },
-    // postKeyAndEmil: async () => {
-    //     const r = await supertest(`${urls.mailboxlayer}`).post(`api/check?${params.key}&${params.email}`).set("Accept", "application/json");
-    //     return r;
-    // },
     post: async () => {
             const r = await supertest(`${urls.mailboxlayer}`).post(`api/check?${params.key}&${params.email}`).set("Accept", "application/json");
             return r;
-            // const a = await supertest(`${urls.mailboxlayer}`).post(`api/check?${params.email}`).set("Accept", "application/json");
-            // return a;
-            //
-            // const b = await supertest(`${urls.mailboxlayer}`).post(`api/check?${params.key}&${params.email}`).set("Accept", "application/json");
-            // return b;
         },
 };
 
--- specs/homework10.spec.js
+++ specs/homework10.spec.js
@@ -3,29 +3,11 @@ import params from "../framework/config/data";
 
 describe("Проверка сервиса Mailboxlayer", () => {
     test("Авторизация с ключом и email / post 200", async () => {
-        // const key = new URLSearchParams(
-        //     {
-        //         "key": "8cd9dd0b86d17e92be16e9d69e18a84d",
-        //     },
-        // );
-        // const email = new URLSearchParams(
-        //     {
-        //         "email": 'lyarullina500@gmail.com',
-        //     },
-        // );
-        // const r = await api().Mailboxlayer().postKeyAndEmil(); // key,email
-        // expect(r.status).toEqual(200);
         const r = await api().Mailboxlayer().post(params.key, params.email);
         expect(r.status).toEqual(200);
     });
     //     Первый тест - положительный
     test("Авторизация с ключом / post 200", async () => {
-        // const key = new URLSearchParams(
-        //     {
-        //         "key": "8cd9dd0b86d17e92be16e9d69e18a84d",
-        //     },
-        // );
-        // const r = await api().Mailboxlayer().postOfKey(); // key
         const r = await api().Mailboxlayer().post(params.key);
         expect(r.status).toEqual(200);
     });
@@ -38,20 +20,11 @@ describe("Проверка сервиса Mailboxlayer", () => {
       ${"8cd9dd0b86d17e92be16e9d64d11!"}    | ${params.email}                  | ${101}
       ${"8cd9dd0b86d17e92be16e9d69e18a84d"} | ${"!2lyarullina500@gma11il.com"} | ${101}
   `("$key & $email = $expected", async ({key, email, expected}) => {
-        // const r = await api().Mailboxlayer().postKeyAndEmil(key,email);
-        // expect(r.status).toEqual(expected);
         const b = await api().Mailboxlayer().post(key,email);
         expect(b.status).toEqual(expected);
     });
     //     Третий тест - проверяет права доступа к точке без api_key
     test("Авторизация с email / post 200", async () => {
-        // const email = new URLSearchParams(
-        //     {
-        //         "email": "8cd9dd0b86d17e92be16e9d69e18a84d",
-        //     },
-        // );
-        // const r = await api().Mailboxlayer().postOfEmail(); // email
-        // expect(r.status).toEqual(200);
         const r = await api().Mailboxlayer().post(params.email);
         expect(r.status).toEqual(200);
     });
